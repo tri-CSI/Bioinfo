0ae71ff352f25a3087539764dc2521e7f4eb76d5
Tri ASUS (personal) <tri.tran@u.nus.edu>
2016-03-11 01:05:49 +0800

    Half done, only interpreting regex, havent match string yet

--- projects/nt/charplay.cpp
+++ projects/nt/charplay.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include "simpleRegex.h"
 
 using namespace std;
 
 int main() {
-    unsigned char string[] = "Amplicon DS";
-    unsigned char match[] = "licon";
+    std::string str_to_search, regex, answer, myans;
+    unsigned char pstr[1000], match[1000];
     
-    std::string myans;
-    std::string answer ="licon";
-    
-    myans = PatternSearch( string, match ); //(const unsigned char* pStr, const unsigned char* pMatch)
-        
-    if (myans == answer) 
-        cout << "PASS: " << myans << endl;
-    else 
-        cout << "FAIL: " << myans << endl;
+    while (true) {
+        cin >> str_to_search;
+        cin >> regex;
+        cin >> answer;
+        std::strcpy( (char*) pstr, str_to_search.c_str() );
+        std::strcpy( (char*) match, regex.c_str() );
+        myans = PatternSearch( pstr, match ); //(const unsigned char* pStr, const unsigned char* pMatch)
+            
+        if (myans == answer) 
+            cout << "PASS: " << myans << endl;
+        else 
+            cout << "FAIL: " << myans << endl;
+        break;
+    }
 }
--- projects/nt/simpleRegex.cpp
+++ projects/nt/simpleRegex.cpp
@@ -1,31 +1,77 @@
 #include <string>
+#define ERROR "ERROR"
 
 std::string RepeatString( const unsigned char ** remaining )
 {
-    std::string result;
+    std::string result = "";
+    std::string repeat_str = "";
+    std::string nestedRep;
+    bool endRepeat = false;
+    int repeat_num = 0;
+
+    while ( **remaining && not endRepeat ) {
+        unsigned char current = *(*remaining)++;
+
+        switch (current) {
+            case '[':
+                nestedRep = RepeatString( remaining );
+                if (nestedRep == ERROR) return ERROR;
+                else repeat_str += nestedRep;
+                break;
+            case ']':
+                endRepeat = true;
+                break;
+            default:
+                repeat_str += current; 
+        }
+    }
+
+    // Next char must be '{'
+    if (*(*remaining)++ != '{') return ERROR;
+
+    // Parse digits of repeat count
+    while ( **remaining != '}') {
+        unsigned char current = *(*remaining)++;
+        if ( current < '0' && current > '9' ) return ERROR;
+        repeat_num = repeat_num * 10 + current - '0';
+    }
+
+    // Next char must be '}'
+    // Should have been checked by previous step...
+    // This step also does the increment before returning to previous function
+    // Can just simply be
+    // *(*remaining)++;
+    if (*(*remaining)++ != '}') return ERROR;
+    
+    // Repeat the string 
+    // This also accept 0 repeat, which is interesting
+    // But why not!
+    while (repeat_num--) result += repeat_str;
+    
     return result;
 }
 
 std::string PatternSearch( const unsigned char * pStr, const unsigned char * pMatch )
 {
-    const std::string error = "ERROR";
     std::string answer = "";
-    std::string expandedMatch = "";
-    int i = 0;       
+    std::string repBlock;
     
-    while ( pMatch[i] ) {
-        unsigned char current = pMatch[i++];
+    while ( *pMatch ) {
+        unsigned char current = *pMatch++;
         switch (current) {
             case '[':
+                repBlock = RepeatString( &pMatch );
+                if (repBlock == ERROR) return ERROR;
+                else answer += repBlock;
+                break;
             case ']':
             case '{':
             case '}':
-                return error;
+                return ERROR;
             default:
-                expandedMatch += current; 
+                answer += current; 
         }
     }
-    answer = expandedMatch;
     
     return answer;
 }
